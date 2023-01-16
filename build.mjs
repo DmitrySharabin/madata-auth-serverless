3b860ba68905e267d6de7afef39a09b4883324c0
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-17 00:25:40 +0100

    On build, generate services.json with a list of supported backends

---
+++ build.mjs
@@ -0,0 +1,31 @@
+import fs from "fs";
+
+let backends, keys;
+try {
+	const data = fs.readFileSync("./backends.json", "utf8");
+	backends = JSON.parse(data);
+} catch (error) {
+	throw new Error("Error reading file backends.json!");
+}
+
+try {
+	const data = fs.readFileSync("./_keys.json", "utf8");
+	keys = JSON.parse(data);
+} catch (error) {
+	throw new Error("Error reading file _keys.json!");
+}
+
+const services = [];
+for (const backend in backends) {
+	const info = { ...backends[backend], ...keys[backend] };
+	if (info.url && info.client_id && info.client_secret) {
+		services.push(backend);
+	}
+}
+
+try {
+	const data = JSON.stringify({ backends: services }, null, "\t");
+	fs.writeFileSync("./public/services.json", data, "utf8");
+} catch (error) {
+	console.error("Error writing file services.json!");
+}
