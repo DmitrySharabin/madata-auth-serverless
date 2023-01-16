faa8ae535c1ac1f0b7706d5286d855c459a969df
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-16 21:11:36 +0100

    Move supported backends to a separate file, so authors could edit them when needed without editing the corresponding serverless function

--- functions/auth.mjs
+++ functions/auth.mjs
@@ -1,30 +1,11 @@
 import fetch from "node-fetch";
 
+import { backends } from "../backends.mjs";
+
 const headers = {
 	"Access-Control-Allow-Origin": "*",
 	"Access-Control-Allow-Headers": "Content-Type",
 	"Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-};
-
-const backends = {
-	Github: {
-		id: "b28e2d03c956b3749798",
-		secret: "",
-		url: "https://github.com/login/oauth/access_token",
-		fields: ""
-	},
-	Google: {
-		id: "380712995757-4e9augrln1ck0soj8qgou0b4tnr30o42.apps.googleusercontent.com",
-		secret: "",
-		url: "https://oauth2.googleapis.com/token",
-		fields: "grant_type=authorization_code"
-	},
-	Dropbox: {
-		id: "2mx6061p054bpbp",
-		secret: "",
-		url: "https://api.dropboxapi.com/oauth2/token",
-		fields: "grant_type=authorization_code"
-	}
 };
 
 export async function handler(event) {
